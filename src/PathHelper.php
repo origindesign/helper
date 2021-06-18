2f9501f19f2f708af6cd614e41c3e627fad4195f
Lindsay Wilson <lwilson@origindesign.ca>
2021-06-18 12:55:41 -0700

    V3.1 - path alias changes

--- src/PathHelper.php
+++ src/PathHelper.php
@@ -2,62 +2,62 @@
 /**
  * @file Contains \Drupal\helper\PathHelper
  */
- 
+
 namespace Drupal\helper;
 
-use Drupal\Core\Path\AliasManager;
+use Drupal\path_alias\AliasManager;
 use Drupal\Core\Path\CurrentPathStack;
-use Drupal\Core\Path\AliasStorage;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 
- 
+
 /**
  * A Class for helping in rendering elements in templates
  *
  */
 class PathHelper {
 
-    protected $pathStack;
-    protected $aliasManager;
-    protected $aliasStorage;
+  protected $pathStack;
+  protected $aliasManager;
+  protected $entityTypeManager;
 
 
-    /**
-     * Class constructor.
-     * @param CurrentPathStack $pathStack
-     * @param AliasManager $aliasManager
-     * @param AliasStorage $aliasStorage
-     */
-    public function __construct( CurrentPathStack $pathStack, AliasManager $aliasManager, AliasStorage $aliasStorage ) {
-        $this->aliasManager = $aliasManager;
-        $this->pathStack = $pathStack;
-        $this->aliasStorage = $aliasStorage;
+  /**
+   * Class constructor.
+   * @param CurrentPathStack $pathStack
+   * @param AliasManager $aliasManager
+   * @param EntityTypeManagerInterface $entityTypeManager
+   */
+  public function __construct( CurrentPathStack $pathStack, AliasManager $aliasManager, EntityTypeManagerInterface $entityTypeManager ) {
+    $this->aliasManager = $aliasManager;
+    $this->pathStack = $pathStack;
+    $this->entityTypeManager  = $entityTypeManager;
+  }
+
+
+  /**
+   * Return the parent path of the current node
+   */
+  public function getPathParent () {
+
+    // Get current Path
+    $current_path = $this->pathStack->getPath();
+    $current_path_alias = $this->aliasManager->getAliasByPath($current_path);
+
+    // Remove Last item of the path (child page title)
+    $arrPath = explode ("/",$current_path_alias);
+    $lastElement = array_pop($arrPath);
+    $pathParent = implode("/",$arrPath);
+
+    // Check if Alias Path exists
+    $aliasExists = $this->aliasManager->getAliasByPath($pathParent, 'en');
+
+    if ( $aliasExists ){
+      return $pathParent;
+    }else{
+      return $current_path_alias;
     }
 
-    
-    /**
-     * Return the parent path of the current node
-     */
-    public function getPathParent () {
-
-        // Get current Path
-        $current_path = $this->pathStack->getPath();
-        $current_path_alias = $this->aliasManager->getAliasByPath($current_path);
-
-        // Remove Last item of the path (child page title)
-        $arrPath = explode ("/",$current_path_alias);
-        $lastElement = array_pop($arrPath);
-        $pathParent = implode("/",$arrPath);
-
-        // Check if Alias Path exists
-        $aliasExists = $this->aliasStorage->aliasExists($pathParent, 'en');
-
-        if ( $aliasExists ){
-            return $pathParent;
-        }else{
-            return $current_path_alias;
-        }
-
-    }
+  }
 
 
   /**
@@ -66,7 +66,7 @@ class PathHelper {
   public function getNidByPath ($path) {
 
     // Check if Alias Path exists
-    $nodePath = $this->aliasStorage->lookupPathSource($path, 'en');
+    $nodePath = $this->aliasManager->getAliasByPath($path, 'en');
 
     if ( $nodePath ){
       $arrPath = explode ("/",$nodePath);
@@ -78,22 +78,22 @@ class PathHelper {
   }
 
 
-    /**
-     * Return the path based on nid
-     */
-    public function getPathByNid ($nid) {
+  /**
+   * Return the path based on nid
+   */
+  public function getPathByNid ($nid) {
 
-        // Check if Path exists
-        $nodePath = $this->aliasManager->getAliasByPath('/node/'.$nid);
+    // Check if Path exists
+    $nodePath = $this->aliasManager->getAliasByPath('/node/'.$nid);
 
-        if ( $nodePath != '/node/' ){
-            // Return path without leading slash
-            return substr($nodePath,1);
-        }else{
-            return false;
-        }
-
+    if ( $nodePath != '/node/' ){
+      // Return path without leading slash
+      return substr($nodePath,1);
+    }else{
+      return false;
     }
+
+  }
 
 
 
--- src/TaxoHelper.php
+++ src/TaxoHelper.php
@@ -2,14 +2,14 @@
 /**
  * @file Contains \Drupal\helper\TaxoHelper
  */
- 
+
 namespace Drupal\helper;
 
 
 use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Path\AliasManager;
+use Drupal\path_alias\AliasManager;
 
- 
+
 /**
  * A Class for helping in rendering elements in templates
  *
@@ -17,71 +17,71 @@ use Drupal\Core\Path\AliasManager;
 class TaxoHelper {
 
 
-    protected $entityTypeManager;
-    protected $aliasManager;
+  protected $entityTypeManager;
+  protected $aliasManager;
 
 
-    /**
-     * TaxoHelper constructor.
-     * @param EntityTypeManagerInterface $entityTypeManager
-     * @param AliasManager $aliasManager
-     */
-    public function __construct(EntityTypeManagerInterface $entityTypeManager, AliasManager $aliasManager) {
-        $this->entityTypeManager  = $entityTypeManager;
-        $this->aliasManager = $aliasManager;
-    }
+  /**
+   * TaxoHelper constructor.
+   * @param EntityTypeManagerInterface $entityTypeManager
+   * @param AliasManager $aliasManager
+   */
+  public function __construct(EntityTypeManagerInterface $entityTypeManager, AliasManager $aliasManager) {
+    $this->entityTypeManager  = $entityTypeManager;
+    $this->aliasManager = $aliasManager;
+  }
 
-    
-    /**
-     * Return taxonomy terms from a specific vocabulary
-     */
-    public function getTaxoTerms ( $vid, $ignoreTids = NULL ) {
 
-        $terms = $this->entityTypeManager->getStorage('taxonomy_term')->loadTree($vid);
+  /**
+   * Return taxonomy terms from a specific vocabulary
+   */
+  public function getTaxoTerms ( $vid, $ignoreTids = NULL ) {
 
-        if($ignoreTids){
+    $terms = $this->entityTypeManager->getStorage('taxonomy_term')->loadTree($vid);
 
-            $termsSubset = array();
+    if($ignoreTids){
 
-            foreach($terms as $term){
-                if(!in_array($term->tid,$ignoreTids)){
-                    array_push($termsSubset,$term);
-                }
-            }
+      $termsSubset = array();
 
-            return $termsSubset;
-
+      foreach($terms as $term){
+        if(!in_array($term->tid,$ignoreTids)){
+          array_push($termsSubset,$term);
         }
+      }
 
-        return $terms;
-
-    }
-
-
-
-    /**
-     * Return label of a taxonomy term
-     */
-    public function getTaxoName ( $tid ) {
-
-        $term = $this->entityTypeManager->getStorage('taxonomy_term')->load($tid);
-
-        return $term->getName();
+      return $termsSubset;
 
     }
 
+    return $terms;
+
+  }
 
 
-    /**
-     * Return path alias of a taxonomy term
-     */
-    public function getTaxoPathAlias ( $tid ) {
 
-        $alias = $this->aliasManager->getAliasByPath('/taxonomy/term/'.$tid);
+  /**
+   * Return label of a taxonomy term
+   */
+  public function getTaxoName ( $tid ) {
 
-        return $alias;
+    $term = $this->entityTypeManager->getStorage('taxonomy_term')->load($tid);
 
-    }
+    return $term->getName();
+
+  }
+
+
+
+  /**
+   * Return path alias of a taxonomy term
+   */
+  public function getTaxoPathAlias ( $tid ) {
+
+    $alias = $this->aliasManager->getAliasByPath('/taxonomy/term/'.$tid);
+
+    return $alias;
+
+  }
 
 
 
